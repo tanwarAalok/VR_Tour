9f9d80f1cdf6d4e53546d8ab6f708ea0e7db678a
Aalok Tanwar <aalok.singh@vitbhopal.ac.in>
2022-05-05 11:50:46 +0530

    first version

---
+++ Assets/SkyboxManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void SkyboxChange(Material material){
+        RenderSettings.skybox = material;
+    }
+}
